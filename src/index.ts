f9699f9ae9c4ce420c25eac3e0225ab433cb2aca
Nathanael Anderson <nathan@master-technology.com>
2023-03-23 22:12:18 -0600

    Converted to using Electron-vite

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-import { ManifestOptions } from "./objects/ManifestOptions";
-import { FileChunkPart } from "./objects/FileChunkPart";
-import { FileManifest } from "./objects/FileManifest";
-import { ManifestInfo } from "./objects/ManifestInfo";
-import { Manifest } from "./objects/Manifest";
-
-export {
-  Manifest,
-  FileManifest,
-  ManifestInfo,
-  FileChunkPart,
-  ManifestOptions,
-}
+export { ManifestOptions } from "./objects/ManifestOptions";
+export { FileChunkPart } from "./objects/FileChunkPart";
+export { FileManifest } from "./objects/FileManifest";
+export { ManifestInfo } from "./objects/ManifestInfo";
+export { Manifest } from "./objects/Manifest";
